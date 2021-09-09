07575be84b2127f957c4d29aeea065414d67c553
derrekcoleman <80121818+derrekcoleman@users.noreply.github.com>
2021-09-08 18:23:05 -0700

    Adding token-gated functionality!

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
 import 'nextra-theme-docs/style.css'
+import React, { useEffect, useState } from 'react'
+import getMember from '../getMember.js'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [member, setMember] = useState(0)
+
+  useEffect(async function () {
+    setMember(await getMember())
+  }, [])
+
+  if (!member) {
+    return <h1>Loading...</h1>
+  }
+
+  if (member) {
+    return (
+      <div>
+        <Component {...pageProps} />
+      </div>
+    )
+  }
 }
